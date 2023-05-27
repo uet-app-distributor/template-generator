50b3e8a70f40879d387df2ede5d99655d431ff66
Thai Nguyen <thainm.uet@gmail.com>
2023-05-27 23:11:31 +0700

    Update Docker generator

--- generators/dockerfile_generator.py
+++ generators/dockerfile_generator.py
@@ -1,21 +1,37 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+TEMPLATE_DIR = 'templates'
+DOCKERFILE_TEMPLATES = {
+    'node': "nodejs-dockerfile.j2"
+}
+
+
+def prepare_template_environment():
+    global template_env
+    template_env = Environment(
+        loader=FileSystemLoader(TEMPLATE_DIR),
+        autoescape=select_autoescape()
+    )
 
 
 def prepare_template_vars(runtime, config):
-  if runtime == 'node':
-    vars = {
-      'version': config['appSpec']['version']
-    }
-  return vars
+    if runtime == 'node':
+        vars = {
+            'version': config['appSpec']['version']
+        }
+    return vars
+
 
 def save_output_to_file(output, file_name):
-  with open(file_name, "w") as file:
-    file.write(output)
+    with open(file_name, "w") as file:
+        file.write(output)
+
 
 def generate_dockerfile(config):
-  runtime = config['appSpec']['image']
-  dockerfile_template = DOCKERFILE_TEMPLATES[runtime]
-  template = template_env.get_template(dockerfile_template)
-  template_vars = prepare_template_vars(runtime, config)
-  dockerfile = template.render(template_vars)
-  save_output_to_file(dockerfile, f"{config['project']}-app-dockerfile")
+    prepare_template_environment()
+    runtime = config['appSpec']['image']
+    dockerfile_template = DOCKERFILE_TEMPLATES[runtime]
+    template = template_env.get_template(dockerfile_template)
+    template_vars = prepare_template_vars(runtime, config)
+    dockerfile = template.render(template_vars)
+    save_output_to_file(dockerfile, f"{config['project']}-app-dockerfile")
--- main.py
+++ main.py
@@ -1,37 +1,19 @@
 import sys
 import yaml
 from yaml.loader import SafeLoader
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 from generators import dockerfile_generator
 
-TEMPLATE_DIR = 'templates'
-DOCKERFILE_TEMPLATES = {
-  'node': "nodejs-dockerfile.j2"
-}
-
-
-def prepare_template_environment():
-  global template_env
-  template_env = Environment(
-    loader=FileSystemLoader(TEMPLATE_DIR),
-    autoescape=select_autoescape()
-  )
 
 def load_yaml_config(config_file):
-  with open(config_file, 'r') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-  return config
-  
-
-def main():
-  if len(sys.argv) > 2:
-    print(f"Generator accepts only 1 argument, got ${len(sys.argv) - 1}.")
-    sys.exit(2)
-  else:
-    prepare_template_environment()
-    config = load_yaml_config(sys.argv[1])
-    generate_dockerfile(config)
+    with open(config_file, 'r') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+    return config
 
 
 if __name__ == "__main__":
-  main()
+    if len(sys.argv) > 2:
+        print(f"Generator accepts only 1 argument, got ${len(sys.argv) - 1}.")
+        sys.exit(2)
+    else:
+        config = load_yaml_config(sys.argv[1])
+        dockerfile_generator.generate_dockerfile(config)
